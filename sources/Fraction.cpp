fb678aa4fcd1587d5976366d307a6030595ebec9
Lara Abu Hamad <laraabuhamad@gmail.com>
2023-04-15 13:37:39 +0300

    creating project

---
+++ sources/Fraction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lara Abu Hamad on 15/04/2023.
+//
+
+#include "Fraction.hpp"
---
+++ sources/Fraction.hpp
@@ -0,0 +1,25 @@
+//
+// Created by Lara Abu Hamad on 15/04/2023.
+//
+
+#ifndef CPP_HW3_FRACTION_H
+#define CPP_HW3_FRACTION_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+
+namespace ariel {
+    class Fraction {
+    private:
+        int a;
+        int b;
+        void reduce;
+    public:
+
+    };
+}
+
+#endif //CPP_HW3_FRACTION_H
